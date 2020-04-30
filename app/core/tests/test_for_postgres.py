6e9fab3c7f1351dd6a58dedffc230cdd4576e506
ashok <ashok.kavungal@tesa.com>
2020-04-30 20:55:45 +0200

    added unit test for helper command to avoid postgres conn fail

---
+++ app/core/tests/test_for_postgres.py
@@ -0,0 +1,26 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandsTestCase(TestCase):
+    """ test cases for helper commands to avoid django tries to connect ,before postgres is ready"""
+    
+    def test_wait_for_db_ready(self):
+        """mocking condition where postgres is ready when django tries to connect"""
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True ##sets the connectionn handlerr return value to succesful
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=None)
+    def test_wait_for_db(self, ts):
+        """simulate conddition where postgres is not ready for the first 5 connect request"""
+
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True] ##returns error for first 5 times
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
