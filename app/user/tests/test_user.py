e5f1bf6528fa339f00dc50386cc553cabb1aea87
ashok <ashok.kavungal@tesa.com>
2020-05-01 03:22:29 +0200

    added user -serializer, view,urls

---
+++ app/user/tests/test_user.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    """function to create new user"""
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """Test creating using with a valid payload is successful"""
+        payload = {
+            'email': 'test@ashok.com',
+            'password': 'test123',
+            'name': 'ashok',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(
+            user.check_password(payload['password'])
+        )
+        self.assertNotIn('password', res.data) ##make sure password is not send via url
+
+    def test_user_exists(self):
+        """Test fails if the payload contains existing user data"""
+
+        payload = {'email': 'test@gmail.com', 'password': '123open'}
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_password_too_short(self):
+        """Test that password must be more than 5 characters"""
+        payload = {'email': 'test@ashok.com', 'password': 'pw'}
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exists = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exists)
